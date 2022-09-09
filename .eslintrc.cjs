8506cdcc952ee76164c658c5579ef7172b5d206d
Alan Poulain <contact@alanpoulain.eu>
2022-09-09 19:27:06 +0200

    feat: modernize codebase

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json']
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+};
